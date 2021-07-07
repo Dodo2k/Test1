47b6640505b18955056265d0a1b5c49516748fd1
Dodo2k <viswatmakamath@gmail.com>
2021-07-07 19:09:45 +0530

    Dated 07-07-2021. Added python client file. Current method of sending data is a GET request with the text data speciafied in the url. Next step is to do the same using a POST request.

--- src/main.cpp
+++ src/main.cpp
@@ -2,34 +2,40 @@
 #include<ESP8266WebServer.h>
 #include<Arduino.h>
 
-const char* ssid = "ESP_AP";
-const char* pass = "password";
+const char* ssid = "MathrukripaG";
+const char* pass = "MathrukripaG574!48";
 
-String pa[10000];
+String pa[3000];
+
 ESP8266WebServer server(80);
 
 void varhandler(){
+  Serial.println("Client Handler invoked....");
   if ( server.hasArg("pa") ){
     String pa = {server.arg("pa")};
-    server.send( 200, "text/html" , "Data received" );
+    server.send( 200, "text/html" , pa );
+    Serial.print("String data recieved\t");
+    Serial.println(pa);
   }
 
 }
 
 void setup(){
-  Serial.begin(115200);
-  delay(1000);
-  WiFi.softAP(ssid,pass);
 
+  Serial.begin(115200);
+  WiFi.begin(ssid,pass);
   server.begin();
+
   while( WiFi.status() != WL_CONNECTED){
     delay(500);
     Serial.println("Waiting to connect.....");
   }
+  
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
   server.on("/data/" , HTTP_GET , varhandler );
+// server.on("/data/" , varhandler );
   server.begin();
   Serial.println("Server listening.....");
   //if server recieves a request with /data in the 
@@ -39,6 +45,6 @@ void setup(){
 
 void loop(){
   server.handleClient();
+  
 
 }
-
